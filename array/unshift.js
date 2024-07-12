5ecd39e6ca29613c02aadef37b831f6afa9b9b29
ajay1931 <ajayprakash032@gmail.com>
2024-07-12 14:16:10 +0530

    updated

---
+++ array/unshift.js
@@ -0,0 +1,5 @@
+let numbers =[1,2,3];
+numbers.unshift(-2,-1,0);
+
+console.log(numbers);  // the elements will add in the beginning of an array
+console.log(numbers.unshift()); //result:6 (array length)
