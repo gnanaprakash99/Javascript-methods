93bd2175e3544d38049b16028f646e2abcc7d087
ajay1931 <ajayprakash032@gmail.com>
2024-07-16 12:32:58 +0530

    join method

---
+++ array/join.js
@@ -0,0 +1,9 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+let newArr1= fruits.join();
+let newArr2= fruits.join(' and ');
+let newArr3= fruits.join("'s ");
+
+console.log(newArr1);
+console.log(newArr2);
+console.log(newArr3);
