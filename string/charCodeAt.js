6c806a13f920c127a5d1e7bbac3a92478260ec39
ajay1931 <ajayprakash032@gmail.com>
2024-07-17 20:37:46 +0530

    charCodeAt method

---
+++ string/charCodeAt.js
@@ -0,0 +1,8 @@
+let letters="ABCD EFGH IJKL MNOP QRST UVWX YZ"
+let Unicode1 = letters.charCodeAt(0)
+let Unicode2 = letters.charCodeAt(2)
+let Unicode3 = letters.charCodeAt(3)
+
+console.log(Unicode1);  //Result =>65
+console.log(Unicode2);  //Result =>67
+console.log(Unicode3);  //Result =>68
