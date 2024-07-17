c0ddccf359aec95cbe8d48dfd8782d9a8b529ebc
ajay1931 <ajayprakash032@gmail.com>
2024-07-17 20:18:27 +0530

    slice method

---
+++ string/slice.js
@@ -0,0 +1,6 @@
+let text="Hello World";
+let result1=text.slice(3,6);  //Result =>lo_
+let result2=text.slice(3);  //Result =>lo World
+let result3=text.slice(0,8); //Result =>Hello_Wo
+
+console.log(result3);
