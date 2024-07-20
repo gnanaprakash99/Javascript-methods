12b9ced40bcdf345448e73cfc977f661a0eb6f83
ajay1931 <ajayprakash032@gmail.com>
2024-07-20 15:24:34 +0530

    remove element method

---
+++ proplems/remove_element.js
@@ -0,0 +1,8 @@
+//Question: Remove duplicates from an array.
+
+let input =['a', 'b', 'a','p','p', 'c'];
+let removeExtraElement=(arr)=>{
+    return console.log([...new Set(arr)]);
+
+};
+removeExtraElement(input)
