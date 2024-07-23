90a2ba6cc50e6fa70af38b35abc971d455df1130
ajay1931 <ajayprakash032@gmail.com>
2024-07-23 14:05:54 +0530

    updated

--- proplems/name_process.js
+++ proplems/name_process.js
@@ -1,23 +1,22 @@
 const names = ["Alice,Ajay,Bob,Charlie,David,Eve,Frank,Kiran,Bhuvan"];
 
+let getNameInOrder = (array) => {
+  let result = {
+    A: [],
+    B: [],
+    C: [],
+    D: [],
+    Others: [],
+  };
 
-let getNameInOrder = (str) => {
-    let splitWord = str.toString().split(',');
-    let firstletters = [];
-    let getWord = null;
-    let result = {};
-    splitWord.forEach(elements => {
-        let firstletter = elements.charAt(0).toLocaleUpperCase();
-        firstletters.push(firstletter);
-    });
-
-    firstletters.forEach(elements => {
-        getWord = splitWord.filter(val => val.startsWith(elements));
-        result[(elements <= "D" ? elements : "Others")] = result[elements <= "D" ? elements : "Others"] || [];
-        result[(elements <= "D" ? elements : "Others")].push(...getWord)
-    });
-    console.log(result);
-
+  let splitWord = array[0].split(",").forEach((element) => {
+    let letters = element[0];
+    if (result[letters]) {
+      result[letters].push(element);
+    } else {
+      result["Others"].push(element);
+    }
+  });
+  console.log("output", result);
 };
-getNameInOrder(names)
-
+getNameInOrder(names);
