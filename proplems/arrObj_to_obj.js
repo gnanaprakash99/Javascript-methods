b9d28ebd9f0340098ad6a91c9aee6be74333d7d8
ajay1931 <ajayprakash032@gmail.com>
2024-07-25 16:12:03 +0530

    updated

--- proplems/arrObj_to_obj.js
+++ proplems/arrObj_to_obj.js
@@ -12,7 +12,19 @@ const input = [
     });
     console.log(result);
   };
-  arrayObjectToObject(input);
+//   arrayObjectToObject(input);
   
 
   // result =>{ '1': 'John', '2': 'Jane', '3': 'Joe' }
+
+  const sales = [
+    { product: "Laptop", sales: 1500 },
+    { product: "Phone", sales: 800 },
+    { product: "Laptop", sales: 1200 },
+    { product: "Tablet", sales: 600 }
+  ];
+result={}
+  sales.forEach(items=>{
+result[items.product]=items.sales
+  });
+  console.log(result);
--- proplems/find_total_amount.js
+++ proplems/find_total_amount.js
@@ -4,24 +4,52 @@ const input = [
     { username: "Alice", amount: 20, date: "2024-01-03" },
     { username: "Bob", amount: 40, date: "2024-01-04" },
     { username: "Alice", amount: 30, date: "2024-01-05" },
-  ];
-  let result = {};
-  let seperateUsers = [];
-  input.forEach((users) => {
+];
+let result = {};
+let seperateUsers = [];
+input.forEach((users) => {
     let { username } = users;
     if (!seperateUsers[username]) {
-      seperateUsers[username] = [];
-      // console.log(users);
+        seperateUsers[username] = [];
+        // console.log(users);
     }
     seperateUsers[username].push(users);
-  });
-  for (let username in seperateUsers) {
+});
+for (let username in seperateUsers) {
     result[username] = seperateUsers[username].reduce(
-      (sum, item) => sum + item.amount,
-      0
+        (sum, item) => sum + item.amount,
+        0
     );
-  }
-  // console.log(seperateUsers);
-  console.log(result);
+}
+// console.log(seperateUsers);
+//   console.log(result);
 
 //   Result =>{ Alice: 100, Bob: 70 }
+
+
+
+// Finding total hours
+
+const workLogs = [
+    { employee: "John", hours: 5 },
+    { employee: "Jane", hours: 8 },
+    { employee: "John", hours: 7 },
+    { employee: "Jane", hours: 6 }
+];
+
+let totalHoursOfAPerson = (arrObj) => {
+    let result = {};
+    let seperateEmployee = [];
+    arrObj.forEach(items => {
+        let { employee, hours } = items;
+        if (!seperateEmployee[employee]) {
+            seperateEmployee[employee] = []
+        }
+        seperateEmployee[employee].push(hours)
+    });
+    for (let employee in seperateEmployee) {
+        result[employee] = seperateEmployee[employee].reduce((sum, item) => sum + item, 0)
+    }
+    console.log(result)
+};
+totalHoursOfAPerson(workLogs)
